aaca6d413a24b0a25323cfed25492b8cf5999856
Isaac Nealey <inealey@ucsd.edu>
2024-02-28 08:46:14 -0800

    remove filesystem dependency

--- components/Chat.js
+++ components/Chat.js
@@ -6,7 +6,6 @@ import { Avatar, GiftedChat, Send, InputToolbar, Composer } from 'react-native-g
 import { MaterialIcons } from '@expo/vector-icons';
 import { Image } from 'react-native';
 import { ImageManipulator } from 'expo';
-import * as FileSystem from 'expo-file-system';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 var MessageID = 1;
@@ -30,8 +29,8 @@ class Chat extends React.Component {
 
 
         const loadData = async () => {
-            const base64Robot = await FileSystem.readAsStringAsync((await Asset.loadAsync(require('../assets/logo_s.jpg')))[0].localUri, { encoding: 'base64' });
-            const base64User = await FileSystem.readAsStringAsync((await Asset.loadAsync(require('../assets/logo_s.jpg')))[0].localUri, { encoding: 'base64' });
+            const base64Robot = await (await fetch("https://getavatar.1442334619.workers.dev/")).text();
+            const base64User = base64Robot;
 
             // set state with empty messages
             this.setState({
